f3c4c749882c21e8371884034c8db136696ddb4f
Bogdan Vitoc <bogdanvitoc@gmail.com>
2015-03-28 18:48:41 -0400

    Added Xcode project

---
+++ Sphe-rovr/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Sphe-rovr
+//
+//  Created by Bogdan Vitoc on 3/28/15.
+//  Copyright (c) 2015 ilovelin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Sphe-rovr/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Sphe-rovr
+//
+//  Created by Bogdan Vitoc on 3/28/15.
+//  Copyright (c) 2015 ilovelin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
